90a205ec81e73daafdd6d7f3dfd0247520f11fb3
Razanoz <61136014+fahmirazan@users.noreply.github.com>
2023-07-10 10:08:34 +0700

    Add files via upload

---
+++ stta.py
@@ -0,0 +1,150 @@
+import os
+import streamlit as st
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from streamlit_drawable_canvas import st_canvas
+import firebase_admin
+from firebase_admin import credentials, storage
+from datetime import datetime
+
+firebase_app = None
+if not firebase_admin._apps:
+    cred = credentials.Certificate("tabangundatar-d2050-firebase-adminsdk-l0l6z-f91317de63.json")
+    firebase_app = firebase_admin.initialize_app(cred, {'storageBucket': 'tabangundatar-d2050.appspot.com'})
+else:
+    firebase_app = firebase_admin.get_app()
+
+bucket = storage.bucket(app=firebase_app)
+
+# Daftar file model .h5
+model_files = {
+    'Model 1 Adam': 'model1_adam.h5',
+    'Model 2 SGD': 'model2_sgd.h5',
+    'Model 3 Rmsprop': 'model3_rmsprop.h5'
+}
+
+selected_model = 'Model 1 Adam'
+
+def load_selected_model():
+    # Memuat model yang dipilih
+    model_file = model_files[selected_model]
+    model = load_model(model_file)
+    return model
+
+class_names = ['jajargenjang', 'lingkaran', 'segiempat', 'segitiga', 'trapesium'] 
+
+def preprocess_image(image):
+    # Convert gambar ke RGB
+    image_rgb = image.convert("RGB")
+
+    # Konversi gambar menjadi array numpy
+    img_array = np.array(image_rgb)
+
+    # Preprocessing gambar
+    processed_image = img_array / 255.0
+
+    # Tambahkan dimensi batch ke gambar
+    input_image = np.expand_dims(processed_image, axis=0)
+
+    return input_image
+
+def predict_image(image, model):
+    # Preprocess gambar
+    input_image = preprocess_image(image)
+
+    # Prediksi kelas gambar menggunakan model
+    predictions = model.predict(input_image)
+    predicted_class = np.argmax(predictions[0])
+
+    return class_names[predicted_class]
+
+def save_to_firebase(image_pil, model_name, class_name):
+    # Membuat direktori temp jika belum ada
+    os.makedirs("temp", exist_ok=True)
+
+    # Membuat nama file dengan format: model_bangundatar_datetime.png
+    current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = f"{model_name}_{class_name}_{current_datetime}.png"
+
+    # Mendapatkan nama folder model berdasarkan model_name
+    model_folder = model_name.replace(" ", "_")
+
+    # Mendapatkan nama folder klasifikasi berdasarkan class_name
+    class_folder = class_name.replace(" ", "_")
+
+    # Menyimpan gambar sebagai file sementara
+    temp_file_path = f"temp/{file_name}"
+    image_pil.save(temp_file_path)
+
+    # Menyimpan file ke Firebase Storage di folder yang sesuai
+    blob = bucket.blob(f"{model_folder}/{class_folder}/{file_name}")
+    blob.upload_from_filename(temp_file_path)
+    blob.make_public()
+    file_url = blob.public_url
+
+    # Menghapus file sementara
+    os.remove(temp_file_path)
+
+    return file_name, file_url
+
+def main():
+    st.title("Aplikasi Klasifikasi Gambar Bangun Datar")
+    st.write("Silakan menggambar gambar di bawah ini.")
+
+    # Memilih model
+    selected_model = st.selectbox("Pilih Model:", ['Pilih Model'] + list(model_files.keys()))
+
+    if selected_model != 'Pilih Model':
+        # Memuat model yang dipilih
+        model = load_selected_model()
+        st.write(f"Model yang dipilih: {selected_model}") 
+           
+    class_name = st.selectbox("Pilih Jenis Bangun Datar:", class_names)
+    st.write(f"Bangun Datar yang dipilih: {class_name}")
+
+    # Membuat canvas untuk menggambar
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 255, 255, 0.8)",  # Warna latar belakang
+        stroke_width=3,  # Ketebalan garis
+        stroke_color="#000000",  # Warna garis
+        background_color="#FFFFFF",  # Warna latar belakang canvas
+        width=300,  # Lebar canvas
+        height=300,  # Tinggi canvas
+        drawing_mode="freedraw",  # Mode menggambar (freedraw: bebas)
+        key="canvas"
+    )
+
+    if st.button("Klasifikasikan"):
+        if selected_model == 'Pilih Model':
+            st.write("Mohon pilih model yang valid.")
+        elif canvas_result.image_data is not None and np.any(canvas_result.image_data != 255):
+            # Ubah data gambar dari canvas menjadi objek PIL.Image
+            image_pil = Image.fromarray(canvas_result.image_data)
+
+            # Tampilkan gambar yang dihasilkan dari canvas
+            st.image(image_pil, caption='Gambar yang Dihasilkan', use_column_width=True)
+
+            # Prediksi kelas gambar
+            predicted_class = predict_image(image_pil, model)
+
+            # Tampilkan hasil prediksi
+            st.write(f"Model yang Digunakan: {selected_model}")
+            st.write(f"Jenis Bangun Datar yang Dipilih: {class_name}")
+            st.write(f"Hasil Klasifikasi: {predicted_class}")
+
+            # Cek apakah hasil klasifikasi sesuai dengan pilihan bangun datar
+            if predicted_class != class_name:
+                st.write(f"Gambar seharusnya termasuk dalam kategori {class_name}.")
+
+            # Simpan gambar ke Firebase Storage
+            file_name, file_url = save_to_firebase(image_pil, selected_model, class_name)
+            st.write(f"Gambar berhasil disimpan di Firebase Storage dengan nama: {file_name}")
+            st.write(f"URL Gambar: {file_url}")
+
+        else:
+            st.write("Canvas kosong, tidak ada gambar yang dihasilkan dari canvas.")
+
+if __name__ == "__main__":
+    main()
